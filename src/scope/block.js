a02940c4f43f693be6647ea4cb2aa9bf0778b3c0
Patatatat <andres.guluarte@gmail.com>
2022-10-19 21:58:20 -0600

    block.js finished

--- src/scope/block.js
+++ src/scope/block.js
@@ -3,6 +3,10 @@ function fruits(){
     var fruit1 = 'Apple';
     let fruit2 = 'Banana';
     const fruit3 = 'Kiwi';
+    console.log(fruit2);
+    console.log(fruit3);
   }
   console.log(fruit1);
 }
+
+fruits();
